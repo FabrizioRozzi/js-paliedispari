8d2cbafad01aa081e3c2170ea98546750a5052b4
FabrizioRozzi <rozzi458@gmail.com>
2021-04-14 14:32:26 +0200

    finito parola palindroma

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var parola = prompt("Inserisci una parola..");
+
+checkPalindroma(parola);
+
+function checkPalindroma(arr)
+{
+   var flag = true;
+   var j = 0;
+    for( var i = arr.length-1; i > arr.length / 2; i-- )
+    {
+        if( arr[i] != arr[j] )
+        {
+           flag = false;
+           break; // Esco dal loop perché é le lettere non sono uguali
+        }
+        j++;
+    }
+  if( flag ) {
+    console.log('La parola inserita é palindroma'); // Se dal loop la flag diventa true stampo questo messaggio
+  }
+  else {
+    console.log('La parola inserita non é palindroma'); // Se dal loop la flag diventa false stampo questo messaggio
+  }
+}
+
+
+
